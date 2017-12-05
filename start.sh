d80c1f1fc21efa425320fc87752aeec3006d5191
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-12-05 20:39:21 +0100

    regexfix

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# ddg-cli v0.8
+# ddg-cli v0.9
 # Made by Dr. Waldijk
 # DuckDuckGo cli.
 # Read the README.md for more info, but you will find more info here below.
 # By running this script you agree to the license terms.
 # Config ----------------------------------------------------------------------------
 DDGNAM="ddg-cli"
-DDGVER="0.8"
+DDGVER="0.9"
 DDGCOL=$(tput cols)
 DDGCOL=$(expr $DDGCOL - 10)
 DDGFLG=$1
@@ -26,15 +26,16 @@ if [[ "$DDGFLG" = "version" ]]; then
     echo "$DDGNAM v$DDGVER"
 else
     if [[ "$DDGFLG" = "results" && -n "$DDGOPT" ]]; then
-        DDGVIEW=$(echo "2*$DDGOPT" | bc)
+        DDGVIEW=$(echo "3*$DDGOPT" | bc)
     else
-        DDGVIEW="10"
+        DDGVIEW="15"
     fi
     read -p "> " DDGSRCH
     echo ""
     DDGSRCH=$(echo $DDGSRCH | sed -r 's/ /+/')
     # Search result
-    DDGRSLT=$(lynx -dump -nolist "https://duckduckgo.com/?q=$DDGSRCH&t=hw&ia=web" | tail -n +13 | sed -r 's/       ([a-z-]+\.[a-z]{2,8}.+)/https:\/\/\1/g' | sed -r '/   [A-Za-z]/d' | sed -r 's/   ([0-9]{1,2}\.)/\1/g' | sed -r '/^$/d' | sed -n "1,$DDGVIEW p")
+    DDGRSLT=$(lynx -dump -nolist "https://duckduckgo.com/?q=$DDGSRCH&t=hw&ia=web" | tail -n +13 | cut -c 4- | sed -r 's/(^[A-Za-z]+.*)/    \1/g' | sed -r 's/(^    [A-Za-z]+.*)$\n/\1/g' | sed -r 's/^    ([a-z]+-?[a-z]+?\.[a-z]+\/?.*)/\1/' | sed '/^    .*$/d' | sed -r 's/^([a-z]+-?[a-z]+?\.[a-z]+\/?.*)/https\:\/\/\1/' | sed -n "1,$DDGVIEW p")
+#    DDGRSLT=$(lynx -dump -nolist "https://duckduckgo.com/?q=$DDGSRCH&t=hw&ia=web" | tail -n +13 | sed -r 's/       ([a-z-]+\.[a-z]{2,8}.+)/https:\/\/\1/g' | sed -r '/   [A-Za-z]/d' | sed -r 's/   ([0-9]{1,2}\.)/\1/g' | sed -r '/^$/d' | sed -n "1,$DDGVIEW p")
 #    DDGRSLT=$(lynx -dump -nolist "https://duckduckgo.com/?q=$DDGSRCH&t=hw&ia=web" | tail -n +13 | sed -r 's/^ + //g' | \
 #    sed -r 's/^([0-9]{1,2}\.  .*)/\1\./g' | \
 #    sed -r 's/(^.+\.[a-z]{2,8}.+\/[A-Za-z]+|^.+\.[a-z]{2,8}\/[A-Za-z]+.+\)|^.+\.[a-z]{2,8}\/[a-z]+.+\/|^.*\.[a-z]{2,8})$/https:\/\/\1/g' | \
@@ -42,7 +43,7 @@ else
 #    sed -r 's/(https.*)$/\n\1/g' | \
 #    tail -n +2 | sed -n "1,$DDGVIEW p")
     # Format text
-    echo "$DDGRSLT" | \
+    echo "$DDGRSLT"
     # ([0-9]{1,2}\.  .+ - [a-zA-Z]+\. |[0-9]{1,2}\.  .+ \| .+[a-zA-Z]+\. |[0-9]{1,2}\.  [a-zA-Z]+ - .+[a-zA-Z]+ [a-zA-Z]+\. )[a-zA-Z]+
     # ([0-9]{1,2}\.  .+ - [a-zA-Z]+\. |[0-9]{1,2}\.  .+ \| .+[a-zA-Z]+\. |[0-9]{1,2}\.  [a-zA-Z]+ - .+[a-zA-Z]+ [a-zA-Z]+\. )[a-zA-Z]
     # ([0-9]{1,2}\.  .+ - [a-zA-Z]+\.|[0-9]{1,2}\.  .+ - .+[a-zA-Z]+ [a-zA-Z]+\.|[0-9]{1,2}\.  .+ \| .+[a-zA-Z]+\.) [a-zA-Z]
@@ -52,7 +53,7 @@ else
 #    fmt -w $DDGCOL | \
 #    sed -r 's/^    ([0-9]{1,2}\.  )/\1/g' | \
 #    sed -r '/^x$/d' | \
-    sed -r 's/(https:.*)/\1\n/g' | head -n -1
+#    sed -r 's/(https:.*)/\1\n/g' | head -n -1
     echo ""
     read -p "> " -s -n1 DDGKEY
     if [[ "$DDGKEY" -ge "1" && "$DDGKEY" -le "5" ]]; then
